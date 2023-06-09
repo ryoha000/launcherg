2b1d58760d0fb93213d45aeea4d7ca059d74273e
ryoha000 <the7uya@gmail.com>
2023-06-10 07:52:27 +0900

    feat: 🎸 getting started

--- src/views/Home.svelte
+++ src/views/Home.svelte
@@ -3,6 +3,8 @@
   import Logo from "/logo.png";
   import { link } from "svelte-spa-router";
   import LinkText from "@/components/UI/LinkText.svelte";
+  import { sidebarCollectionElements } from "@/store/sidebarCollectionElements";
+  import ButtonCancel from "@/components/UI/ButtonCancel.svelte";
 
   const memoRegex = /^smde_memo-(\d+)$/;
   const memoPromises = Promise.all(
@@ -11,6 +13,8 @@
       .filter((v) => v)
       .map((v) => commandGetCollectionElement(v))
   );
+
+  let isOpenGettingStarted = true;
 </script>
 
 <div class="p-8 space-y-8">
@@ -18,6 +22,21 @@
     <img src={Logo} alt="launcherg logo" class="h-12" />
     <div class="text-(text-tertiary h3) font-bold">Hgame laucher</div>
   </div>
+  {#if $sidebarCollectionElements.length !== 0 && isOpenGettingStarted}
+    <div
+      class="space-y-2 p-4 border-(border-primary solid ~) rounded max-w-120"
+    >
+      <div class="flex items-center">
+        <div class="text-(text-primary h3) font-medium">Getting started</div>
+        <div class="ml-auto">
+          <ButtonCancel on:click={() => (isOpenGettingStarted = false)} />
+        </div>
+      </div>
+      <div class="text-(text-tertiary body)">
+        持っているゲームをこのランチャーに登録してみましょう。左のサイドバーにある緑色の「Add」ボタンから自動で追加できます。
+      </div>
+    </div>
+  {/if}
   <div class="space-y-2">
     <div class="text-(text-primary h3) font-medium">Help</div>
     <LinkText href="https://google.com" text="TODO: おきかえ" />
