76a26bdfad053f990519acae0c8f6518f6e37590
ryoha000 <the7uya@gmail.com>
2023-09-17 00:09:31 +0900

    feat: 🎸 sidebar の入力クリアボタン

--- src/components/Sidebar/SearchInput.svelte
+++ src/components/Sidebar/SearchInput.svelte
@@ -27,7 +27,7 @@
   class="border-(2px solid transparent) focus-within:border-accent-accent rounded transition-all"
 >
   <div
-    class="group w-full flex items-center gap-2 px-2 py-1 border border-(border-primary solid) rounded bg-bg-secondary hover:bg-bg-primary focus-within:(border-transparent bg-bg-primary) transition-all"
+    class="group w-full flex items-center gap-2 px-2 py-1 border border-(border-primary solid) rounded bg-bg-secondary hover:bg-bg-primary focus-within:(border-transparent bg-bg-primary) transition-all relative"
   >
     <div class="w-5 h-5 i-material-symbols-search color-text-primary" />
     <input
@@ -36,5 +36,11 @@
       {placeholder}
       class="w-full text-(body2 text-primary) bg-bg-secondary group-hover:bg-bg-primary focus:bg-bg-primary placeholder-text-placeholder transition-all"
     />
+    {#if value !== ""}
+      <button
+        on:click={() => (value = "")}
+        class="absolute right-2 w-5 h-5 i-material-symbols-cancel-outline-rounded color-text-primary"
+      />
+    {/if}
   </div>
 </div>
