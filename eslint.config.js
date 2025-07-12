e8c004665fb64229798e3f7074d3532cbea9e55e
ryoha000 <the7uya@gmail.com>
2025-07-13 04:17:22 +0900

    src-tauri/gen/** を eslint の対象外に

--- eslint.config.js
+++ eslint.config.js
@@ -1,7 +1,7 @@
 import antfu from '@antfu/eslint-config'
 
 export default antfu({
-  ignores: ['script', '**/script/**'],
+  ignores: ['script', '**/script/**', 'src-tauri/gen/**'],
   svelte: true,
   rules: {
     'prefer-const': 'off',
