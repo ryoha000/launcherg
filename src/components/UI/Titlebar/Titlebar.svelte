770dbb6ddd9074a697010d4e9c6dc6b19b359e73
ryoha000 <the7uya@gmail.com>
2025-07-14 21:09:01 +0900

    style: format

--- src/components/UI/Titlebar/Titlebar.svelte
+++ src/components/UI/Titlebar/Titlebar.svelte
@@ -1,7 +1,6 @@
 <script lang='ts'>
   import { goto } from '@mateothegreat/svelte5-router'
   import { onMount } from 'svelte'
-  import ButtonIcon from '@/components/UI/ButtonIcon.svelte'
   import ControlButton from '@/components/UI/Titlebar/ControlButton.svelte'
   import Close from '@/components/UI/Titlebar/Icons/Close.svelte'
   import Maximize from '@/components/UI/Titlebar/Icons/Maximize.svelte'
@@ -26,7 +25,7 @@
 <div class='grid grid-cols-[1fr_auto] items-center h-8 bg-[#202020] text-(text-primary)'>
   <div data-tauri-drag-region class='h-full'></div>
   <div class='ml-auto flex items-center h-full'>
-    <button class="h-7 w-7 flex items-center justify-center rounded-1 bg-transparent transition-all" aria-label="Open Settings" onclick={navigateToSettings}>
+    <button class='h-7 w-7 flex items-center justify-center rounded-1 bg-transparent transition-all' aria-label='Open Settings' onclick={navigateToSettings}>
       <div class="w-5 h-5 {isFocused() ? 'color-[#e5e5e5' : 'color-[#797979]'} i-material-symbols-settings-outline-rounded"></div>
     </button>
     <ControlButton variant='normal' isFocused={isFocused()} onclick={minimize}>
